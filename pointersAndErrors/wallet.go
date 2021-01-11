8984410f6af5c13d8d3195d8d90c4c5b3f320fda
a970747 <Matt.J.Jackson@gmail.com>
2021-01-11 13:58:54 -0500

    Start Pointers & Errors

---
+++ pointersAndErrors/wallet.go
@@ -0,0 +1,24 @@
+package wallet
+
+import "fmt"
+
+type Bitcoin int
+type stringer interface {
+	String() string
+}
+
+type Wallet struct {
+	balance Bitcoin
+}
+
+func (w *Wallet) Deposit(amount Bitcoin) {
+	w.balance += amount
+}
+
+func (w Wallet) Balance() Bitcoin {
+	return w.balance
+}
+
+func (b Bitcoin) String() string {
+	return fmt.Sprintf("%d BTC", b)
+}
