8ed78aba07ba309c2da951478cd446535a4d9f73
a970747 <Matt.J.Jackson@gmail.com>
2021-01-11 09:59:42 -0500

    Create struct, create/add methods, implement interface. Table based test functionality

--- structsMethodsInterfaces/perimeter.go
+++ structsMethodsInterfaces/perimeter.go
@@ -1,17 +1,39 @@
 package perimeter
 
-//Rectangle new type for Rectangle
+import "math"
+
+//Rectangle new type for rectangle info
 type Rectangle struct {
 	Width  float64
 	Height float64
 }
 
-//Perimeter returns perimeter of a shape given 2 sides
-func Perimeter(rectangle Rectangle) float64 {
-	return (rectangle.Height + rectangle.Width) * 2
+//Perimeter Perim of a rectangle given two sides
+func (r Rectangle) Perimeter() float64 {
+	return (r.Width + r.Height) * 2
 }
 
-//Area returns area of a shape given 2 sides
-func Area(rectangle Rectangle) float64 {
-	return (rectangle.Height * rectangle.Width)
+//Area Area of a rectangle given two sides
+func (r Rectangle) Area() float64 {
+	return r.Width * r.Height
+}
+
+//Circle new type for circle info
+type Circle struct {
+	Radius float64
+}
+
+//Area Area of a circle given struct's radius
+func (c Circle) Area() float64 {
+	return math.Pi * c.Radius * c.Radius
+}
+
+//Triangle Area of a triangle given a side
+type Triangle struct {
+	Side float64
+}
+
+//Area Area of a circle given struct's radius
+func (t Triangle) Area() float64 {
+	return (t.Side * t.Side) / 2
 }
--- structsMethodsInterfaces/perimeter_test.go
+++ structsMethodsInterfaces/perimeter_test.go
@@ -1,12 +1,14 @@
 package perimeter
 
-import (
-	"testing"
-)
+import "testing"
+
+type Shape interface {
+	Area() float64
+}
 
 func TestPerimeter(t *testing.T) {
 	rectangle := Rectangle{10.0, 10.0}
-	got := Perimeter(rectangle)
+	got := rectangle.Perimeter()
 	want := 40.0
 
 	if got != want {
@@ -15,11 +17,38 @@ func TestPerimeter(t *testing.T) {
 }
 
 func TestArea(t *testing.T) {
-	rectangle := Rectangle{10.0, 10.0}
-	got := Area(rectangle)
-	want := 100.00
+	areaTests := []struct {
+		name    string
+		hasArea float64
+		shape   Shape
+	}{
+		{name: "Rectangle", shape: Rectangle{Width: 12, Height: 6}, hasArea: 72.0},
+		{name: "Circle", shape: Circle{Radius: 10}, hasArea: 314.1592653589793},
+		{name: "Triangle", shape: Triangle{Side: 10}, hasArea: 50.0},
+	}
 
-	if got != want {
-		t.Errorf("got %.2f want %.2f", got, want)
+	for _, tt := range areaTests {
+		got := tt.shape.Area()
+		if got != tt.hasArea {
+			t.Errorf("%#v got %g want %g", tt.shape, got, tt.hasArea)
+		}
 	}
 }
+
+/* 	checkArea := func(t testing.TB, shape Shape, want float64) {
+	t.Helper()
+	got := shape.Area()
+	if got != want {
+		t.Errorf("got %g want %g", got, want)
+	}
+}
+
+t.Run("rectangles", func(t *testing.T) {
+	rectangle := Rectangle{12, 6}
+	checkArea(t, rectangle, 72.0)
+})
+
+t.Run("circles", func(t *testing.T) {
+	circle := Circle{10}
+	checkArea(t, circle, 314.1592653589793)
+}) */
