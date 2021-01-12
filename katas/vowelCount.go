30270cf7ce7abadcba2d766cf89255f065674344
a970747 <Matt.J.Jackson@gmail.com>
2021-01-12 14:12:56 -0500

    Add the given kata function

--- katas/vowelCount.go
+++ katas/vowelCount.go
@@ -4,7 +4,7 @@ import "fmt"
 
 //https://www.codewars.com/kata/54ff3102c1bad923760001f3/go
 
-func main() {
+func vowelCount(str string) (count int) {
 	for _, c := range str {
 		switch c {
 		case 'a', 'e', 'i', 'o', 'u':
