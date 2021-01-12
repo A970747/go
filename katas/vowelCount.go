f8bdc9b312aa244c81ffb3069df0eaf59fdc2eb1
a970747 <Matt.J.Jackson@gmail.com>
2021-01-12 14:11:41 -0500

    Vowel Count improved

--- katas/vowelCount.go
+++ katas/vowelCount.go
@@ -5,20 +5,10 @@ import "fmt"
 //https://www.codewars.com/kata/54ff3102c1bad923760001f3/go
 
 func main() {
-	for i := 0; i < len(str); i++ {
-		char := (string([]rune(str)[i]))
-		switch char {
-		case "a":
+	for _, c := range str {
+		switch c {
+		case 'a', 'e', 'i', 'o', 'u':
 			count++
-		case "e":
-			count++
-		case "i":
-			count++
-		case "o":
-			count++
-		case "u":
-			count++
-		default:
 		}
 	}
 	fmt.Println(count)
